f98903de714f80f39f4f9141880b738ee53088e8
munozcar20 <carlosmz243@gmail.com>
2022-01-25 19:04:14 -0500

    Se agregan preguntas faltantes

--- preguntas.js
+++ preguntas.js
@@ -17,12 +17,12 @@ objetos preguntas */
 
 const p1 = new Pregunta(1,"¿Cuál es la ciudad más poblada del mundo?", "Ciudad de Mexico", "Bogotá", "Sao Paulo", "Tokio");
 const p2 = new Pregunta(2,"¿A qué edad murió Abraham Lincoln?", "32", "48", "56", "64");
-const p3 = new Pregunta(3,"¿3?", "1", "2", "3", "4");
-const p4 = new Pregunta(4,"¿4?", "", "", "", "");
-const p5 = new Pregunta(5,"¿5?", "", "", "", "");
-const p6 = new Pregunta(6,"¿6?", "", "", "", "");
-const p7 = new Pregunta(7,"¿7?", "", "", "", "");
-const p8 = new Pregunta(8,"¿8?", "", "", "", "");
-const p9 = new Pregunta(9,"¿9?", "", "", "", "");
-const p10 = new Pregunta(10,"¿10?", "", "", "", "");
+const p3 = new Pregunta(3,"¿Cuál es el nombre del río más largo del mundo?", "Río Nilo", "Río Grande", "Río Danubio", "Río Amazonas");
+const p4 = new Pregunta(4,"¿Cuál es el país que tiene forma de bota?", "Portugal", "Honduras", "España", "Italia");
+const p5 = new Pregunta(5,"¿Cuál es la capital de Nicaragua?", "Santiago", "Brasilia", "Ciudad de Nicaragua", "Managua");
+const p6 = new Pregunta(6,"¿Cuál es la única ciudad que está en dos continentes distintos?", "Berlín", "Moscú", "Viena", " Estambul");
+const p7 = new Pregunta(7,"¿Cuál es la cascada más alta del mundo?", "Cataratas del Niáraga", "Cataratas Victoria", "Cataratas de Iguazú", "El Salto del Ángel");
+const p8 = new Pregunta(8,"¿En qué año llegó Cristobal Colón a América?", "1490", "1592", "1429", "1492");
+const p9 = new Pregunta(9,"¿Quién es el padre del psicoanálisis?", "William James", "Skinner", "Carl Gustav Jung", "Sigmund Freud");
+const p10 = new Pregunta(10,"¿En qué país se usó la primera bomba atómica?", "China", "Rusia", "Estados Unidos", "Japón");
 //Creamos los objetos (preguntas)
